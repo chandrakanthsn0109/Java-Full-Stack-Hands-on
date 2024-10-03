d8127c27ff49f0dcc8cc03b6324ec2780bb5101d
Nagaraju <chandrakanth.nagaraju@biworldwide.com>
2024-10-03 18:27:50 +0530

    Second JavaScript Assignment

---
+++ JavaScript/Module.js
@@ -0,0 +1,12 @@
+const name = "Jesse";
+const age = 40;
+
+export {name, age};
+
+export function add(a,b){
+    return a+b;
+}
+
+export function sub(a,b){
+    return a-b;
+}
---
+++ JavaScript/main.js
@@ -0,0 +1,6 @@
+import {name,age,add,sub} from "./Module.js"
+console.log("Name:"+name);
+console.log("Age:"+age);
+
+console.log("Sum:"+add(10,7));
+console.log("Subtraction:"+sub(10,7));
