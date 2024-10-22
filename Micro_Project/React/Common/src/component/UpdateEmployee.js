f0e05fdda2dc6965b0611a043626345ff64e63af
Nagaraju <chandrakanth.nagaraju@biworldwide.com>
2024-10-22 23:36:55 +0530

    React Micro_Projects

---
+++ Micro_Project/React/Common/src/component/UpdateEmployee.js
@@ -0,0 +1,135 @@
+import { Component } from 'react';
+import "./Employee.css";
+import Employee from '../model/Employee';
+import axios from "axios";
+export default class UpdateEmployee extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            flag: false,
+            eid: "",
+            ename: "",
+            esalary: "",
+            employee: "",
+            eidError:"",
+            enameError:"",
+            esalaryError:""
+        }
+
+    }
+
+    validateEid = (eid) => {
+        let reg = /^[0-9]+$/
+        if (eid === "") {
+            return "Employee Id is Required"
+        }
+        else if (!reg.test(eid)) {
+            return "Invalid Employee Id"
+        }
+        else
+            return null
+
+    }
+    validateEname = (ename) => {
+        let reg = /^[a-zA-Z]+$/
+        if (ename === "") {
+            return "Employee Name is Required"
+        }
+        else if (!reg.test(ename)) {
+            return "Invalid Employee Name"
+        }
+        else
+            return null
+
+    }
+    validateEsalary = (esalary) => {
+        let reg = /^[0-9]+$/
+        if (esalary === "") {
+            return "Employee Salary is Required"
+        }
+        else if (!reg.test(esalary)) {
+            return "Invalid Employee Salary"
+        }
+        else
+            return null
+
+    }
+
+    changeId = (e) => {
+        this.setState({ eid: e.target.value });
+        let error = this.validateEid(this.state.eid);
+        this.setState({eidError:error})
+
+    }
+    changeName = (e) => {
+        this.setState({ ename: e.target.value });
+        let error = this.validateEname(this.state.ename);
+        this.setState({enameError:error})
+    }
+    changeSalary = (e) => {
+        this.setState({ esalary: e.target.value });
+        let error = this.validateEsalary(this.state.esalary);
+        this.setState({esalaryError:error})
+    }
+    GetResult = (e) => {
+        e.preventDefault();
+        let error = this.validateEid(this.state.eid);
+        this.setState({eidError:error})
+
+        let error1 = this.validateEname(this.state.ename);
+        this.setState({enameError:error1})
+
+        let error2 = this.validateEsalary(this.state.esalary);
+        this.setState({esalaryError:error2})
+
+        if(!error&&!error1&&!error2){
+            this.setState({ flag: true });
+            let employee = new Employee();
+            employee.setId(this.state.eid);
+            employee.setName(this.state.ename);
+            employee.setSalary(this.state.esalary);
+
+            axios.put("http://localhost:3004/employees/"+this.state.eid, employee)
+                .then(() => {
+                    document.getElementById("res").innerHTML = "<b>Object Updated Successfully</b>";
+                })
+            e.preventDefault();
+        }
+    }
+    render() {
+        return (
+            <div  id="myForm">
+                <div class="container" >
+                <h1 align="center">Employee Mangement System</h1>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                    <label for="first" class="fw-bold">Enter Employee Id:</label>
+                    <input type='text' value={this.state.eid} onChange={this.changeId} class="form-control border border-info"></input>
+                    <font color="red"><b>{this.state.eidError}</b></font>
+                    </div>
+                    <div class="form-group">
+                    <label for="second" class="fw-bold">Enter Employee Name:</label>
+                    <input type='text' value={this.state.ename} onChange={this.changeName} class="form-control border border-info"></input>
+                    <font color="red" id="in"><b>{this.state.enameError}</b></font>
+                    </div>
+                    <div class="form-group">
+                    <label for="third" class="fw-bold">Enter Employee Salary:</label>
+                    <input type='text' value={this.state.esalary} onChange={this.changeSalary} id="in" class="form-control border border-info"></input>
+                    <font color="red" id="in"><b>{this.state.esalaryError}</b></font>
+                    </div>
+                    <button onClick={this.GetResult} id="bt" class="btn btn-success">Update</button>&nbsp;
+                    <br></br><br></br>
+                </form>
+                {this.state.flag ?
+                
+                    <div id="res">
+                        
+                    </div>
+                    : ''
+                }
+                </div>
+            </div>
+        );
+    }
+}
---
+++ Micro_Project/React/Individual/src/model/Employee.js
@@ -0,0 +1,22 @@
+class Employee {
+    id;
+    name;
+    salary;
+    constructor() {
+       this.id=0;
+       this.name="";
+       this.salary=0;
+
+    }
+
+    setId(id){
+       this.id=id;
+    }
+    setName(name){
+       this.name=name;
+    }
+    setSalary(salary){
+       this.salary=salary;
+    }
+}
+export default Employee;
