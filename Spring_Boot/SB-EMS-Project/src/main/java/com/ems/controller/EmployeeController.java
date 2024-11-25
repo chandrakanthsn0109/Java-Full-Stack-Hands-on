274d7fc7f18027f6df0e6a6d38b7f4c94afc6b69
Nagaraju <chandrakanth.nagaraju@biworldwide.com>
2024-11-25 18:39:24 +0530

    Spring Boot

--- Spring_Boot/SB-EMS-Project/src/main/java/com/ems/controller/EmployeeController.java
+++ Spring_Boot/SB-EMS-Project/src/main/java/com/ems/controller/EmployeeController.java
@@ -17,39 +17,63 @@ import com.ems.model.Employee;
 import com.ems.repo.EmployeeRepo;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class EmployeeController {
-	
+
 //	@GetMapping("/hello") //	@RequestMapping(method = RequestMethod.GET)
 //	public String sayHello() {
 //		return "Hello Spring Boot";
 //	}
-	@Autowired
-	EmployeeRepo repo;
 	
+	private EmployeeRepo repo;
+	
+	@Autowired
+	public EmployeeController(EmployeeRepo repo) {
+		this.repo = repo;
+	}
+
 	@PostMapping("/create-employee")
 	public void doInsert(@RequestBody Employee employee) {
 		repo.save(employee);
 	}
-	
+
 	@PutMapping("/update-employee")
 	public void doUpdate(@RequestBody Employee employee) {
 		repo.save(employee);
 	}
-	
+
 	@DeleteMapping("/delete-employee/{empId}")
 	public void doDelete(@PathVariable("empId") int empId) {
 		repo.deleteById(empId);
 	}
-	
+
 	@GetMapping("/read-employee/{empId}")
 	public Employee doRead(@PathVariable("empId") int empId) {
 		return repo.findById(empId).get();
 	}
-	
+
 	@GetMapping("/readAll-employee")
 	public List<Employee> doReadAll() {
 		return ((List<Employee>) repo.findAll());
 	}
+
+	@GetMapping("/find-employee-name/{empName}")
+	public List<Employee> doFindByName(@PathVariable("empName") String empName) {
+		return ((List<Employee>) repo.findByEmpName(empName));
+	}
+
+	@DeleteMapping("/delete-employee-name/{empName}")
+	public void doDeleteByName(@PathVariable("empName") String empName) {
+		repo.deleteByEmpName(empName);
+	}
+
+	@GetMapping("/find-employee-salary/{empSalary}")
+	public List<Employee> doFindBySalary(@PathVariable("empSalary") int empSalary) {
+		return ((List<Employee>) repo.findByEmpSalary(empSalary));
+	}
 	
+	@DeleteMapping("/delete-employee-salary/{empSalary}")
+	public void doDeleteBySalary(@PathVariable("empSalary") int empSalary) {
+		repo.deleteByEmpSalary(empSalary);
+	}
 }
